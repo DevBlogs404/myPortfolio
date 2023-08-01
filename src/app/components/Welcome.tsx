19a31e9b208ee5890637261f8fe0b034e3125a80
shubham <hoodashubham92@gmail.com>
2023-08-02 01:00:40 +0530

    welcome text updated

--- src/app/components/Welcome.tsx
+++ src/app/components/Welcome.tsx
@@ -6,13 +6,10 @@ export default function Welcome() {
       <div className="text-6xl md:text-8xl pointer-events-none">Welcome.</div>
       <div className='my-6 p-4 pointer-events-none'>
         <p className=" text-gray-300 break-words">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore
-          aliquam eaque illum ipsum sapiente ab magnam minus expedita
-          possimus nulla?
+        My name is Shubham, I'm a front-end developer based in Haryana, IN. I have developed many types of front-ends from well known  Ecommerce platforms to  Modren Websites.
         </p>
         <p className="mt-4 text-gray-300 break-words">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil,
-          ab!
+        I'm passionate about cutting-edge, pixel-perfect, beautiful interfaces and intuitively implemented UX
         </p>
       </div>
     </div>
